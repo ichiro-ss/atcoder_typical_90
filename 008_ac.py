20da903f0af3e54bbfd149ab6925fe6e4b1d9966
ichiro-ss <sei.swmr@gmail.com>
2022-05-05 15:25:08 +0900

    solved 8

---
+++ 008_ac.py
@@ -0,0 +1,18 @@
+# input
+n = int(input())
+s = str(input())
+target = "atcoder"
+mod = 10 ** 9 + 7
+
+# dp[i][j]: use till s[i] and make till target[j]
+dp = [[0 for _ in range(len(target)+1)] for _ in range(n+1)]
+for i in range(n+1):
+    dp[i][0] = 1
+
+for i in range(1, n+1):
+    for j in range(1, len(target)+1):
+        if s[i-1] == target[j-1]:
+            dp[i][j] = int((dp[i-1][j-1] + dp[i-1][j]) % mod)
+        else:
+            dp[i][j] = dp[i-1][j]
+print(int(dp[n][len(target)] % mod))
