6e9156b5a6f31fe8f72b28546f57c636c741e6ea
ichiro-ss <sei.swmr@gmail.com>
2022-05-21 00:26:33 +0900

    solved 52

--- 052_dp.py
+++ 052_dp.py
@@ -2,5 +2,11 @@
 n = int(input())
 a = []
 for _ in range(n):
-    a.append([int(i) for i in input().split()])
+    a.append([int(_) for _ in input().split()])
 
+num = 10 ** 9 + 7
+ans = 1
+for i in range(n):
+    ans *= sum(a[i])
+    ans %= num
+print(ans % num)
