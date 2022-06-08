d40f5f42a5c2ea44af06bb72c635416a4f562ca1
ichiro-ss <sei.swmr@gmail.com>
2022-06-08 22:29:00 +0900

    modified 007

--- 007_cc.py
+++ 007_cc.py
@@ -1,20 +1,19 @@
+from bisect import bisect_left
+
 # input
 n = int(input())
-a = [int(i) for i in input().split()]
+a = [int(_) for _ in input().split()]
 q = int(input())
 b = []
 for _ in range(q):
     b.append(int(input()))
 
-a.sort
-l, r = 0, n - 1
+a.sort()
 for i in range(q):
-    while l < r + 1:
-        mid = l + (r - l) // 2
-        if a[mid] == b[i]:
-            break
-        elif a[mid] < b[i]:
-            l = mid
-        else:
-            r = mid - 1
-    print(min(abs(a[mid] - b[i]), abs(a[mid-1] - b[i])))
+    idx = bisect_left(a, b[i])
+    if idx == 0:
+        print(abs(a[idx]-b[i]))
+    elif idx == n:
+        print(abs(a[idx-1]-b[i]))
+    else:
+        print(min(abs(a[idx-1]-b[i]), abs(a[idx]-b[i])))
